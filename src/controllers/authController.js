33b352ff10010b839881a72fdb2e1f08b273ce78
Renan Cavalcanti <renan_cavalcanti@hotmail.com>
2026-04-01 15:31:57 -0400

    chore: list of users

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -8,6 +8,15 @@ const generateToken = (id, email) => {
     return jwt.sign({id, email}, jwtSecret, {expiresIn: "1d"});
 }
 
+const serializeUser = (user) => ({
+    id: user._id,
+    name: user.name,
+    email: user.email,
+    createdAt: user.createdAt,
+    updatedAt: user.updatedAt
+
+});
+
 const register = async (req, res) => {
     try{
         console.log(req.body);
@@ -140,4 +149,31 @@ const getMe = async (req, res) => {
     
 }
 
-module.exports = { register, login, getMe };
+const listUsers = async (req, res) => {
+    try{
+        if(!req.user || !req.user.id){
+            return res.status(401).json({
+                message: "Unauthorized."
+            });
+        }
+
+        const users = await User.find({_id: {$ne: req.user.id}})
+            .select("-password")
+            .sort({name: 1});
+        
+        return res.json({
+            message: "Users fetched successfully.",
+            data: {
+                users: users.map(serializeUser)
+            }
+        });
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).json({
+            message: "Error while fetching users."
+        });
+    }
+}
+
+module.exports = { register, login, getMe, listUsers };
